c18f660f5be0eb5ede4f9866c2474ab61e6eb27e
Athina <athina.kantis@gmail.com>
2024-11-02 09:08:55 +0200

    add assets

--- src/Components/SelectPlan/PlanCard.jsx
+++ src/Components/SelectPlan/PlanCard.jsx
@@ -15,7 +15,7 @@ function PlanCard(props) {
         onClick={(e) => handleSelect(e)}
         className={`planCard ${props.gamePlan === props.plan ? 'active' : 'inactive'}`} 
         >
-        <img src={`/src/assets/images/icon-${props.plan}.svg`} alt={props.alt} />
+        <img src={`src/assets/images/icon-${props.plan.toLowerCase()}.svg`} alt={props.alt} />
         <div className="description">
         <p className="title">{props.plan}</p>
         <p className="pricing">{!props.paymentPlan ? `$${props.monthPrice}/mo` : `$${props.yearPrice}/yr`}</p>

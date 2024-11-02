33de61f1946b8d631195a94f9fd53b8b4fe82ed6
Athina <athina.kantis@gmail.com>
2024-11-02 13:01:49 +0200

    correct filepaths

--- src/Components/Confirmation/Confirmation.jsx
+++ src/Components/Confirmation/Confirmation.jsx
@@ -3,7 +3,7 @@ import './Confirmation.css'
 function Confirmation() {
     return (
         <section id="confirmation">
-            <img src="src/assets/images/icon-thank-you.svg" alt="Checkmark icon" />
+            <img src="assets/assets/images/icon-thank-you.svg" alt="Checkmark icon" />
             <h2>Thank you!</h2>
             <p>  Thanks for confirming your subscription! We hope you have fun 
             using our platform. If you ever need support, please feel free 
